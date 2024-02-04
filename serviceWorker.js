1a0685dc84bb832383f9389958a097dabd2385e4
anasvhora284 <vhoraayaz8@gmail.com>
2024-02-04 15:17:38 +0530

    fixed vite config for pwa

---
+++ serviceWorker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("my-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/static/js/main.js",
+        "/static/css/main.css",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "auto",
+      manifest: {
+        name: "Invoice Generator",
+        short_name: "InGen",
+        description:
+          "This is an invoice generator app which can be used to print digital version of your invoice.",
+        start_url: "/home/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+      },
+    }),
+  ],
 });
